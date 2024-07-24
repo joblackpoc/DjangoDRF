c5798a7fee365008662970e8c8d96a2e6f104d26
MASTERPOP-N <duopoc@gmail.com>
2024-07-24 12:17:30 +0700

    modified static in templates

--- crud/views.py
+++ crud/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
-    templatefilename = 'crud/index.html'
+    templatefilename = 'crud/sharp-home.html'
     return render(request, templatefilename)
