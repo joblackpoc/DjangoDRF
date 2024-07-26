4044dbaaf879014e75f343baa49a56e3a63426d7
joblackpoc <duopoc@gmail.com>
2024-07-26 16:18:30 +0700

    test post data from model

--- company/views.py
+++ company/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from . models import Post
 # Create your views here.
 def Home(request):
+    welcome = Post.objects.all()
     templatefilename = 'company/home.html'
-    return render(request, templatefilename)
+    context = {
+        'welcome':welcome
+    }
+    return render(request, templatefilename, context)
