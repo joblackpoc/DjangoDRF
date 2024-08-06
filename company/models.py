351bc3830bd98f47b246ba658ed1fd364659d86a
MASTERPOP-N <duopoc@gmail.com>
2024-08-06 14:47:03 +0700

    Register model

--- company/models.py
+++ company/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     post_title = models.CharField(max_length=60)
     post_content = models.TextField()
     published_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.post_title
--- project/settings.py
+++ project/settings.py
@@ -77,25 +77,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+}
+#     'default':{
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'dbpayroll',
+#         'USER':'adminpoc',
+#         'PASSWORD':'123456',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+
+#         }   
 #     }
 # }
-    'default':{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'dbpayroll',
-        'USER':'adminpoc',
-        'PASSWORD':'123456',
-        'HOST':'localhost',
-        'PORT':'3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-
-        }   
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
